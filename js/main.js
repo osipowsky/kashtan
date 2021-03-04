c2d5b9963c6fb29eb79601d4ece48798f10f6366
osipowsky <71651812+osipowsky@users.noreply.github.com>
2021-03-04 14:12:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1 @@
+new Swiper('.swiper-container',{pagination:{el:'.swiper-pagination',clickable:true,},breakpoints:{320:{slidesPerView:1,slidesPerGroup:1,},820:{slidesPerView:2,slidesPerGroup:2,},},});
